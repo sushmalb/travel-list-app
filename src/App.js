96668d62fe2db819d73521bb06d839db433bc6ab
Sushma <sushma.l.b@gmail.com>
2024-06-19 18:46:48 +0530

    updated form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
-}
+};
+
+const Logo = () => {
+  return <h1> Explore</h1>;
+};
+
+const Form = () => {
+  const [desc, setDesc] = useState(" ");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { desc, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setQuantity(1);
+    setDesc(" ");
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Trip Essentials</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((x) => (
+          <option value={x} key={x}>
+            {x}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="items..."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((i) => (
+          <Item item={i} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description}
+        {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const Stats = () => {
+  return <footer> Items in your list are:</footer>;
+};
 
 export default App;
